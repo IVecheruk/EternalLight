7f972361fcaf0aea7f8de2c1d3e9b6606966e62a
Lausan99 <vecheruk.99@gmail.com>
2026-02-06 06:28:55 +0300

    Initial commit

---
+++ src/main/java/com/eternallight/backend/EternalLightApplication.java
@@ -0,0 +1,13 @@
+package com.eternallight.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EternalLightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EternalLightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eternallight/backend/EternalLightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eternallight.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EternalLightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
